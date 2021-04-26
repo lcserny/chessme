314da1682b1c7ef28e4599c1fc650214f55bf51e
Leonardo Cserny <leo.cserny@gmail.com>
2021-04-26 19:07:24 +0300

    minor fix

--- src/errors.ts
+++ src/errors.ts
@@ -1,26 +1,26 @@
 export class GameStatusError extends Error {
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = "GameStatusError";
     }
 }
 
 export class NoPlayersError extends Error {
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = "NoPlayersError";
     }
 }
 
 export class TooManyPlayersError extends Error {
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = "TooManyPlayersError";
     }
 }
 
 export class SamePlayerTeamError extends Error {
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = "SamePlayerTeamError";
     }
