1a74c4a0b75fb18ebe4d67572491a3e3ba2c4c16
Leonardo Cserny <leo.cserny@gmail.com>
2021-05-01 16:01:00 +0300

    adds todo

--- tests/outcome.test.ts
+++ tests/outcome.test.ts
@@ -2,7 +2,15 @@ import {assert, expect} from "chai";
 import {describe, it} from "mocha";
 
 describe("outcome scenarios", function () {
-    it("", function () {
+    it("pawn cannot move 2 locations", function () {
+        // TODO
+    });
 
+    it("pawn can move 1 location", function () {
+        // TODO
+    });
+
+    it("pawn moves 1 location over enemy and defeats it", function () {
+        // TODO
     });
 });
