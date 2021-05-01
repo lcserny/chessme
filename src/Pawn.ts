eb94ebbc758d874fd629a120f0c3505600cf65e2
Leonardo Cserny <leo.cserny@gmail.com>
2021-05-01 23:19:48 +0300

    minor refactor

--- src/Pawn.ts
+++ src/Pawn.ts
@@ -4,8 +4,33 @@ import {Location, Positions, Row} from "./Position";
 export class Pawn extends Piece {
 
     availableMovesBlack(currentLocation: Location, positions: Positions): Array<Location> {
-        // TODO
-        return new Array<Location>();
+        let moves = new Array<Location>();
+
+        let belowOne = currentLocation.down();
+        let belowOnePos = positions.getPosition(belowOne);
+        if (belowOnePos == null || !belowOnePos.hasPiece()) {
+            moves.push(belowOne);
+        }
+
+        let belowTwo = belowOne.down();
+        let belowTwoPos = positions.getPosition(belowTwo);
+        if (currentLocation.row == Row.SEVEN && (belowTwoPos == null || !belowTwoPos.hasPiece())) {
+            moves.push(belowTwo);
+        }
+
+        let belowLeft = belowOne.left();
+        let belowLeftPos = positions.getPosition(belowLeft);
+        if (belowLeftPos != null && belowLeftPos.hasPiece() && belowLeftPos.piece.playerColor != this.playerColor) {
+            moves.push(belowLeft);
+        }
+
+        let belowRight = belowOne.right();
+        let belowRightPos = positions.getPosition(belowRight);
+        if (belowRightPos != null && belowRightPos.hasPiece() && belowRightPos.piece.playerColor != this.playerColor) {
+            moves.push(belowRight);
+        }
+
+        return moves;
     }
 
     availableMovesWhite(currentLocation: Location, positions: Positions): Array<Location> {
