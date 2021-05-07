4ac42a3d653bd12d81f5b8e3125411ff33ada4ce
Leonardo Cserny <leo.cserny@gmail.com>
2021-05-07 12:28:27 +0300

    impl bidirectional UDP comm

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,20 @@
-import dgram from "dgram";
+import dgram, {RemoteInfo} from "dgram";
 
 // UDP client example
-let socket = dgram.createSocket("udp4");
-socket.bind(function () {
-    socket.setBroadcast(true);
+let client = dgram.createSocket("udp4");
+client.on("error", function (err: Error) {
+    console.log("client error:n" + err.stack);
+    client.close();
+});
+client.on("message", function (msg: Buffer, rinfo: RemoteInfo) {
+    console.log("client got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+});
+client.on("listening", function () {
+    let address = client.address();
+    console.log("client listening " + address.address + ":" + address.port);
+});
+client.bind(51234, function () {
+    client.setBroadcast(true);
 });
 let message = Buffer.from("Hi");
-socket.send(message, 0, message.length, 41234, '255.255.255.255', function (err, bytes) {
-    socket.close();
-});
+client.send(message, 0, message.length, 41234, '255.255.255.255');
